c264339076b6a17b6a8f61a661e0e9b675ba2f0d
Marcio Bueno <marciobueno@gmail.com>
2024-03-07 19:59:59 -0300

    Configurado o express básico + cors

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,5 +1,21 @@
 import "dotenv/config";
+import cors from "cors";
+import express from "express";
 
 console.log("Hello ever running Node.js project.");
-
 console.log("MY_SECRET", process.env.MY_SECRET);
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("My express server!");
+});
+
+app.post("/", (req, res) => {
+  res.send("My express server - POST!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
