eca0f74f4efc0a79a1ebb074a5766aad668344f3
Sabrina Costa <114702957+sabrinnawin@users.noreply.github.com>
2024-04-15 20:59:34 +0000

    Atualizado

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,17 +1,28 @@
+import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import { models } from "./models";
-import userRoutes from "./userRoutes";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
+  req.context = { models, me: models.users[1] };
   next();
 });
 
-app.use("/", userRoutes);
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
-export default app;
+app.get("/", (req, res) => {
+  return res.send("Hello Express!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
